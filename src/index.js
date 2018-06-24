00bf96d6a6248cbea7e476daca2d702f61920183
Hitoshi Okawa <14all.okawa@gmail.com>
2018-06-24 22:43:04 +0900

    Add post resolver

--- src/index.js
+++ src/index.js
@@ -6,17 +6,25 @@ let links = [{
   description: 'Fullstack tutorial for GraphQL'
 }]
 
+let idCount = links.length
+
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
   },
 
-  Link: {
-    id: (root) => root.id,
-    description: (root) => root.description,
-    url: (root) => root.url,
-  }
+  Mutation: {
+    post: (root, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    }
+  },
 }
 
 const server = new GraphQLServer({
